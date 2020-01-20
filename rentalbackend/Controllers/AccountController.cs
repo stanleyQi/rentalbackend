3993e8d5f92dec2c76cb4f8c372f74f7e9a9008c
LiQi <qili811227@gmail.com>
2020-01-20 20:17:24 +1300

    Add project files.

---
+++ rentalbackend/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using rentalbackend.Dto;
+using rentalbackend.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rentalbackend.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController:Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            IConfiguration configuration
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<object> Login([FromBody] LoginDto model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var appUser = _userManager.Users.SingleOrDefault(r => r.Email == model.Email);
+                return await JwtHelper.GenerateJwtToken(model.Email, appUser, _configuration);
+            }
+
+            throw new ApplicationException("INVALID_LOGIN_ATTEMPT");
+        }
+
+        [HttpPost]
+        public async Task<object> Register([FromBody] RegisterDto model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Email,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return await JwtHelper.GenerateJwtToken(model.Email, user, _configuration);
+            }
+
+            throw new ApplicationException("UNKNOWN_ERROR");
+        }
+    }
+}
---
+++ rentalbackend/Entities/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace rentalbackend.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            const string databaseName = "rental";
+            const string databaseUser = "root";
+            const string databasePass = "";
+
+            return $"Server=localhost;" +
+                   $"database={databaseName};" +
+                   $"uid={databaseUser};" +
+                   $"pwd={databasePass};" +
+                   $"pooling=true;";
+        }
+    }
+}
